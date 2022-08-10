342405eef090691f705f96807aea360172a159a8
j <j@DESKTOP-73DBTI0>
2022-08-10 13:05:30 +0900

    찜 알림창

--- potatoMarket/src/main/webapp/script/itemView.js
+++ potatoMarket/src/main/webapp/script/itemView.js
@@ -1,6 +1,11 @@
 
 function changeFavo() {
 	$('#command').val('addFavo');
+	if($('#favocode').val()!=-1){
+		alert("이미 찜한 아이템 입니다.");
+	}else{
+		alert("찜 목록에 추가 되었습니다.");
+	}
 	$('form').submit();
 }
 
